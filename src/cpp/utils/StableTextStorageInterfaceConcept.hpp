b19ed5183c2852871995e73da8d19edd7ed4d2c3
Matthew Ték <48158662+Szapi@users.noreply.github.com>
2025-07-24 21:09:32 +0200

    Make StableTextStorageInterface concept variadic

--- src/cpp/utils/StableTextStorageInterfaceConcept.hpp
+++ src/cpp/utils/StableTextStorageInterfaceConcept.hpp
@@ -6,11 +6,19 @@
 namespace tagliatelle::concepts
 {
 
-    template<class T>
-    concept StableTextStorageInterface = requires (T& t, std::string_view sv)
+    namespace _detail
     {
-        { t.Store(sv) } -> std::same_as<std::string_view>;
-        requires std::copyable<T>;
-    };
+
+        template<class T>
+        concept StableTextStorageInterface = requires (T& t, std::string_view sv)
+        {
+            { t.Store(sv) } -> std::same_as<std::string_view>;
+            requires std::copyable<T>;
+        };
+
+    }
+
+    template<class T, class ... Us>
+    concept StableTextStorageInterface = (_detail::StableTextStorageInterface<T> && _detail::StableTextStorageInterface<Us> && ...);
 
 } // namespace tagliatelle::concepts
