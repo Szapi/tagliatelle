e6637d959aaf62241ef7183479334081e4e011dc
Matthew Ték <48158662+Szapi@users.noreply.github.com>
2025-08-01 23:40:56 +0200

    Added SourceTokens to EventPool

--- src/cpp/lib_main/EventPool.hpp
+++ src/cpp/lib_main/EventPool.hpp
@@ -3,16 +3,19 @@
 #include "EventAttributes.hxx"
 #include "AttributeCodec.hxx"
 #include "PagedArray.hpp"
+#include "Permit.hpp"
 
 namespace tagliatelle
 {
-    
+
     // The Event Pool is the main data structure that holds all known events.
     // Events are encoded for efficient storage and filtering.
     template<class...Attrs>
         requires EventDefinition<Attrs...>
     class EventPool
     {
+        SINGLETON(EventPool);
+
         struct UniqueID { std::size_t value; };
         struct SourceID { std::size_t value; };
 
@@ -23,6 +26,47 @@ namespace tagliatelle
             ...
         >;
         static_assert(std::is_trivial_v<EncodedEvent>);
+
+    public:
+        class SourceToken
+        {
+        public:
+            SourceToken(SourceID id, Permit<EventPool<Attrs...>>) : id{id} {}
+
+            // Copyable
+            SourceToken(const SourceToken&) = default;
+            SourceToken& operator=(const SourceToken&) = default;
+
+            // Moveable
+            SourceToken(SourceToken&&) = default;
+            SourceToken& operator=(SourceToken&&) = default;
+
+            // Comparable
+            auto operator<=>(const SourceToken&) const = default;
+
+            SourceID GetID(Permit<EventPool<Attrs...>>) const { return id; }
+            
+        private:
+            SourceID id;
+        };
+
+        // Not thread-safe!
+        [[nodiscard]] SourceToken RegisterEventSource(const std::string_view info)
+        {
+            SourceToken token{registeredEventSources.Size(), Permit{this}};
+            registeredEventSources.Emplace(eventSourceInfoTexts.Store(info));
+            return token;
+        }
+
+    private:
+        inline static constexpr std::size_t EventSourceString_SizeThreshold  = 256u;
+        inline static constexpr std::size_t EventSourceString_BufferPageSize = 4096u;
+
+        PagedArray<std::string_view, 128u> registeredEventSources{};
+        _detail::SizeSensitiveTextStorage<
+            EventSourceString_SizeThreshold,
+            EventSourceString_BufferPageSize
+        > eventSourceInfoTexts;
     };
 
 } // namespace tagliatelle
